acac878820f80d9367ab0044ed6833e009434453
Lea SAGNIER <lea.sagnier@vinci-energies.net>
2022-01-21 18:25:35 +0100

    modal

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
 const api = "https://api.airtable.com/v0/appQPw7ookVvJ3hEb/Books?&api_key=keyjc1nnVReGqYJCF";
-
+const apiAuthor = "https://api.airtable.com/v0/appQPw7ookVvJ3hEb/Authors?&api_key=keyjc1nnVReGqYJCF";
 
 fetch(api)
     .then((res) => res.json())
     .then((data) => {
-        //    console.log(data);
+        //console.log(data);
         printData(data.records);
     });
-
 
 const printData = (data) => {
     data.map((data, i) => {
         const url = data.fields.Cover[0].url;
+        const titre = data.fields.Titre;
+        const id = data.fields.id;
         const message =
             "Titre: " +
-            data.fields.Titre + "\n" +
+            titre + "\n" +
             "Catégories: " +
-            data.fields.Topic + "\n" +
-            "Auteur(s) :" +
-            data.fields.Auteur + "\n";
+            data.fields.Topic + "\n"
 
-        writeData(message, url);
-        console.log(url)
+        writeData(message, url, titre, id);
+        console.log(id)
     });
 };
 
-const writeData = (data, url) => {
+const writeData = (data, url, titre, id) => {
     const newContent = document.createElement("article");
     newContent.innerText = data;
     document.body.appendChild(newContent);
@@ -34,5 +33,10 @@ const writeData = (data, url) => {
     elem.setAttribute('src', url);
     document.body.appendChild(newContent).appendChild(elem);
 
+    $(document).click(function() {
+        $('#modal').css('display', 'flex');
+        $('.titleModalText').text(titre);
+        $('.conentImg').html(elem);
+    });
 
 };
