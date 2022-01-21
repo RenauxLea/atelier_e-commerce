d37da04269c460bd3f57eca07e7ffd5aa0c6fa0f
Lea SAGNIER <lea.sagnier@vinci-energies.net>
2022-01-21 16:51:37 +0100

    img

--- js/script.js
+++ js/script.js
@@ -1,36 +1,38 @@
 const api = "https://api.airtable.com/v0/appQPw7ookVvJ3hEb/Books?&api_key=keyjc1nnVReGqYJCF";
 
+
 fetch(api)
-	.then((res) => res.json())
-	.then((data) => {
-		// console.log(data);
-		printData(data.records);
-	});
+    .then((res) => res.json())
+    .then((data) => {
+        //    console.log(data);
+        printData(data.records);
+    });
+
 
 const printData = (data) => {
-	data.map((data, i) => {
-		const message =
-			"Post #" +
-			i +
-			" : " +
-			data.fields.Titre +
-			", Catégories: " +
-			data.fields.Topic +
-			" Auteur(s) :" +
-			data.fields.Auteur +
-			' <img class="imgtest" src="' +
-			data.fields.Cover[0].url +
-			'">';
-		console.log(data.fields.Cover[0].url);
-		$(".imgtest").attr("src", data.fields.Cover[0].url);
-		var img = $("<img />", { src: data.fields.Cover[0].url });
-		img.appendTo(".img");
-		writeData(message);
-	});
+    data.map((data, i) => {
+        const url = data.fields.Cover[0].url;
+        const message =
+            "Titre: " +
+            data.fields.Titre + "\n" +
+            "Catégories: " +
+            data.fields.Topic + "\n" +
+            "Auteur(s) :" +
+            data.fields.Auteur + "\n";
+
+        writeData(message, url);
+        console.log(url)
+    });
 };
 
-const writeData = (data) => {
-	const newContent = document.createElement("article");
-	newContent.innerText = data;
-	document.body.appendChild(newContent);
+const writeData = (data, url) => {
+    const newContent = document.createElement("article");
+    newContent.innerText = data;
+    document.body.appendChild(newContent);
+
+    var elem = document.createElement('img');
+    elem.setAttribute('src', url);
+    document.body.appendChild(newContent).appendChild(elem);
+
+
 };
