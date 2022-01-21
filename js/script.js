1c73d4970af66eb9ce53c46737fdaa55374f40fb
Lea Renaux <lea.renaux@datalegaldrive.com>
2022-01-21 13:52:55 +0100

    fetch catégories

--- js/script.js
+++ js/script.js
@@ -1,34 +1,32 @@
 const api = "https://api.airtable.com/v0/appQPw7ookVvJ3hEb/Books?&api_key=keyjc1nnVReGqYJCF";
 
 fetch(api)
-    .then((res) => res.json())
-    .then((data) => {
-        // console.log(data);
-        printData(data.records);
-    });
+	.then((res) => res.json())
+	.then((data) => {
+		// console.log(data);
+		printData(data.records);
+	});
 
 const printData = (data) => {
-    data.map((data, i) => {
-        const message =
-            "Post #" +
-            i +
-            " : " +
-            data.fields.Titre +
-            ", Page: " +
-            data.fields.Status +
-            " --- Recipe: " +
-            data.fields.Recipe +
-            " (" +
-            data.fields.Type +
-            ') <img src="' +
-            data.fields.Image +
-            '">';
-        writeData(message);
-    });
+	data.map((data, i) => {
+		const message =
+			"Post #" +
+			i +
+			" : " +
+			data.fields.Titre +
+			", Statut: " +
+			data.fields.Topic +
+			" (" +
+			data.fields.Auteur +
+			') <img src="' +
+			data.fields.Image +
+			'">';
+		writeData(message);
+	});
 };
 
 const writeData = (data) => {
-    const newContent = document.createElement("article");
-    newContent.innerText = data;
-    document.body.appendChild(newContent);
+	const newContent = document.createElement("article");
+	newContent.innerText = data;
+	document.body.appendChild(newContent);
 };
